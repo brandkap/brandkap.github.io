905277347505ae9902ad16256dd2b7593121f993
shinydragonite <49795423+shinydragonite@users.noreply.github.com>
2020-05-24 16:18:42 -0700

    Loading wavey text from codepen (contains javascript, and scss)

---
+++ javascript/wavetext.js
@@ -0,0 +1,12 @@
+console.log("d");
+
+// add class wavetext to html to use this animation on it
+var text = $(".wavetext").text();
+$(".wavetext").html("");
+for (var i in text) {
+  if (text[i] === " ") {
+    $(".wavetext").append($("<span>").html("&nbsp;"));
+  } else {
+    $(".wavetext").append($("<span>").text(text[i]));
+  }
+}
