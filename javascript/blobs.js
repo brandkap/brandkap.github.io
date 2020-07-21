88121fffb8b59927cdfe11fd9e755a23602daa3c
shinydragonite <49795423+shinydragonite@users.noreply.github.com>
2020-07-21 12:03:01 -0700

    case study updates

--- javascript/blobs.js
+++ javascript/blobs.js
@@ -295,7 +295,7 @@
     ctx = screen.ctx;
   screen.resize();
   // create LavaLamps
-  lava0 = new LavaLamp(screen.width, screen.height, 4, "#F7F7F7", "#F7F7F7");
+  lava0 = new LavaLamp(screen.width, screen.height, 5, "#F5F5F5", "#F5F5F5");
 
   run();
 })();
