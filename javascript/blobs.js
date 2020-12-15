456b99e738e396c5740895abc3ffa876c0713829
shinydragonite <49795423+shinydragonite@users.noreply.github.com>
2020-12-14 22:47:10 -0800

    hero content changed, icons too

--- javascript/blobs.js
+++ javascript/blobs.js
@@ -295,7 +295,7 @@
     ctx = screen.ctx;
   screen.resize();
   // create LavaLamps
-  lava0 = new LavaLamp(screen.width, screen.height, 5, "#F7F7F7", "#F7F7F7");
+  lava0 = new LavaLamp(screen.width, screen.height, 5, "#FAFAFA", "#FAFAFA");
 
   run();
 })();
