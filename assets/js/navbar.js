859be0ecf14b54e2ca70a8e9f250a18b15a1d24d
Jeremy Kaplan <jdkaplan@metagram.net>
2021-05-24 20:32:32 -0700

    Sticky navbar

---
+++ assets/js/navbar.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.getElementById("navbar");
+  var oldPos = 0;
+  window.addEventListener("scroll", function () {
+    var newPos = document.documentElement.scrollTop;
+    if (newPos > oldPos) {
+      // Scrolling _down_ the page => hide the nav
+      navbar.style.top = "-100%";
+    } else {
+      // Scrolling _up_ the page => show the nav
+      navbar.style.top = "0";
+    }
+    // Mobile overscroll gives negative positions.
+    oldPos = Math.max(0, newPos);
+  });
+});
